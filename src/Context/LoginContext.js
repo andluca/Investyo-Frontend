a1cd9a6ef5e579be20b2d0bcf60bdc7b70e6c5db
Denilson Fontes <defontescruz@gmail.com>
2022-11-07 03:23:13 -0300

    Api Requests

--- src/Context/LoginContext.js
+++ src/Context/LoginContext.js
@@ -12,16 +12,31 @@ const cache = new Cache({
   backend: AsyncStorage,
 });
 
-cache.set("url", "https://d04b-201-182-107-196.sa.ngrok.io/");
+cache.set("url", "https://7f72-201-182-107-196.sa.ngrok.io/");
 
 export const LoginContext = createContext();
 
 const LoginContextProvider = ({ children }) => {
   const [acessToken, setAcessToken] = useState("");
 
+  const getAcessAxios = async (acessToken) => {
+    const url = await getUrl();
+
+    const baseURL = `${url}user`;
+
+    const acessAxiosInstance = axios.create({
+      baseURL,
+      headers: {
+        authorization: acessToken,
+      },
+    });
+
+    return acessAxiosInstance;
+  };
+
   const addToCache = async (key, value) => {
     try {
-      await cache.remove(key)
+      await cache.remove(key);
       await cache.set(key, value);
     } catch (error) {
       Alert.alert(error + " Cache Failure");
@@ -46,7 +61,104 @@ const LoginContextProvider = ({ children }) => {
     }
   };
 
-  
+  const doLogout = async () => {
+    try {
+      const refreshToken = await getRefreshToken();
+      const acessAxios = await getAcessAxios(acessToken);
+      acessAxios.post("/logout", {
+        refreshToken,
+      });
+    } catch (error) {
+      Alert.alert(error);
+    }
+  };
+
+  const performOperationBuy = async (symbol, quantity) => {
+    try {
+      const acessAxios = await getAcessAxios(acessToken);
+      acessAxios.post("/perform_operation", {
+        operationType: "buyAsset",
+        symbol,
+        quantity
+      });
+    } catch (error) {
+      Alert.alert(error);
+    }
+  };
+
+  const performOperationSell = async (symbol, quantity) => {
+    try {
+      const acessAxios = await getAcessAxios(acessToken);
+      acessAxios.post("/perform_operation", {
+        operationType: "sellAsset",
+        symbol,
+        quantity
+      });
+    } catch (error) {
+      Alert.alert(error);
+    }
+  };
+
+  const addBalance = async (value) => {
+    try {
+      const acessAxios = await getAcessAxios(acessToken);
+      acessAxios.post("/change_balance", {
+        operationType: "addFunds",
+        value
+      });
+    } catch (error) {
+      Alert.alert(error);
+    }
+  };
+
+  const withdrawBalance = async (value) => {
+    try {
+      const acessAxios = await getAcessAxios(acessToken);
+      acessAxios.post("/change_balance", {
+        operationType: "withdrawFunds",
+        value
+      });
+    } catch (error) {
+      Alert.alert(error);
+    }
+  };
+
+  const searchByName = async (name) => {
+    try {
+      const acessAxios = await getAcessAxios(acessToken);
+      return await acessAxios.get(`/api/search_name?name=${name}`);
+    } catch (error) {
+      Alert.alert(error);
+    }
+  };
+
+  const getHistoricalValues = async (days) => {
+    try {
+      const acessAxios = await getAcessAxios(acessToken);
+      return await acessAxios.get(`/api/historical_values?days=${days}`);
+    } catch (error) {
+      Alert.alert(error);
+    }
+  };
+
+  const getProfileWallet = async () => {
+    try {
+      const acessAxios = await getAcessAxios(acessToken);
+      return await acessAxios.get("/profile/wallet");
+    } catch (error) {
+      Alert.alert(error);
+    }
+  };
+
+  const getProfisleExtracts = async () => {
+    try {
+      const acessAxios = await getAcessAxios(acessToken);
+      return await acessAxios.get("/profile/extract");
+    } catch (error) {
+      Alert.alert(error);
+    }
+  };
+
   const doLogin = async (username, password) => {
     try {
       const url = await getUrl();
@@ -59,10 +171,12 @@ const LoginContextProvider = ({ children }) => {
       const AcessToken = result.headers.authorization;
 
       await addToCache("refreshToken", refreshToken);
+
       await setAcessToken(AcessToken);
-      return true
+
+      return true;
     } catch (error) {
-      return false
+      return false;
     }
   };
 
@@ -77,47 +191,57 @@ const LoginContextProvider = ({ children }) => {
   const doRefreshToken = async () => {
     try {
       const url = await getUrl();
-      const refreshToken = await getRefreshToken()
-      
+      const refreshToken = await getRefreshToken();
+
       const result = await axios.post(url + "refresh", {
-        refreshToken
+        refreshToken,
       });
       const newRefreshToken = result.data.refreshToken;
 
       const AcessToken = result.headers.authorization;
 
-
       await addToCache("refreshToken", newRefreshToken);
+
       await setAcessToken(AcessToken);
     } catch (error) {
       Alert.alert(error + " Refresh Failure");
     }
   };
 
-  const doRegister = async(username, password) =>{
+  const doRegister = async (username, password) => {
     try {
       const url = await getUrl();
-      const register = await axios.post(url+'register', {
-        username, 
-        password
-      })
+      Alert.alert(url);
+      const register = await axios.post(url + "register", {
+        username,
+        password,
+      });
 
-      Alert.alert('Usuário '+register.data.user.username+' cadastrado')
-      return true
+      Alert.alert("Usuário " + register.data.user.username + " cadastrado");
+      return true;
     } catch (error) {
+      console.log(error);
       Alert.alert(error + " Register Failure");
-      return false
+      return false;
     }
-  }
+  };
 
   return (
     <LoginContext.Provider
       value={{
-        acessToken,
         connectToBackend,
         getUrl,
         doLogin,
-        doRegister
+        doRegister,
+        doLogout,
+        getProfisleExtracts,
+        getProfileWallet,
+        getHistoricalValues,
+        searchByName,
+        withdrawBalance,
+        addBalance,
+        performOperationBuy,
+        performOperationSell
       }}
     >
       {children}
